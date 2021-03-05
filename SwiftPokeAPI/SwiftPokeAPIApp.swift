85c2598fddb49270789bd185f1cd6c50fb1f2e24
joferava <jfelipe221095@gmail.com>
2021-03-04 20:25:07 -0600

    Initial Commit

---
+++ SwiftPokeAPI/SwiftPokeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPokeAPIApp.swift
+//  SwiftPokeAPI
+//
+//  Created by Felipe Ramirez Vargas on 4/3/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPokeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
